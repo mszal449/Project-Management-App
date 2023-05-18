b0718d0c1581298d79dc31325b2ef7e35040d220
Hortensjaa <lilka305@gmail.com>
2023-05-18 14:37:43 +0200

    chcange field in Current class

--- Classes/Current.java
+++ Classes/Current.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class Current extends Task {
     // poziomy postępu zadania
-    private static final String[] progress_level = {
+    private static final String[] progress_levels = {
             "Rozpoczęto",
             "W trakcie",
             "Wysłano pierwszą wersję",
@@ -16,29 +16,39 @@ public class Current extends Task {
             "Czeka na zatwierdzenie"};
 
     private final LocalDate start_date; // data rozpoczęcia
-    private String progress; // aktualny poziom postępu
+    private int cur_level; // aktualny poziom postępu
 
     // konstruktor tworzący trwające zadanie
     public Current(String name, int day, int month, int year) {
         super(name, day, month, year);
         start_date = LocalDate.now();
-        progress = progress_level[0];
+        cur_level = 0;
     }
 
     // konstruktor tworzący trwające zadanie na podstawie zaplanowanego zadania
     public Current(Planned planned_task) {
         super(planned_task);
         start_date = LocalDate.now();
-        progress = progress_level[0];
+        cur_level = 0;
+    }
+
+    // dostęp do informacji o statusie postępu
+    public String getProgressLevel() {
+        return progress_levels[cur_level];
+    }
+    public void setProgressLevel(int level) {
+        if (level < 5 && level >= 0) {
+            cur_level = level;
+        }
     }
 
     // metoda reprezentująca robienie postępu w zadaniu
+    // TODO: ona w sumie chyba do wywalenia jest
     public void makeProgress() {
-        int index = Arrays.asList(progress_level).indexOf(progress);
         // jeżeli zadanie nie jest jeszcze na ostatnim poziomie
-        if (index < progress_level.length - 1 ) {
+        if (cur_level < progress_levels.length - 1 ) {
             // przenosimy je na wyższy poziom
-            progress = progress_level[index + 1];
+            cur_level += 1;
         }
         // w przeciwnym wypadku
         else {
@@ -47,11 +57,6 @@ public class Current extends Task {
             // i usuwamy stary
             project.deleteTask(this);
         }
-    }
-
-    // dostęp do informacji o statusie postępu
-    public String getProgressLevel() {
-        return progress;
     }
 
     // dostęp do daty rozpoczęcia zadania
@@ -67,6 +72,6 @@ public class Current extends Task {
     // metoda pomocnicza wypisująca informacje o zadaniu do konsoli
     public void getInfo() {
         super.getInfo();
-        System.out.println("status : " + progress);
+        System.out.println("status : " + progress_levels[cur_level]);
     }
 }
